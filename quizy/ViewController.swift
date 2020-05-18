01f22a9bbe508c60354b88a54f3c0c09c698b5e0
Iswandi Saputra <iswandi.saputra11@gmail.com>
2020-05-18 15:04:48 +0700

    Initial Commit

---
+++ quizy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  quizy
+//
+//  Created by Iswandi Saputra on 18/05/20.
+//  Copyright © 2020 group8mc2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
